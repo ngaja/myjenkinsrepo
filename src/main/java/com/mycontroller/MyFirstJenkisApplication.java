97da9cd5f297b58fd97c16ccd72d24a95bcca5a9
Bakari <bchangale@gmail.com>
2018-12-23 16:36:28 +0300

    first commit

---
+++ src/main/java/com/mycontroller/MyFirstJenkisApplication.java
@@ -0,0 +1,14 @@
+package com.mycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstJenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstJenkisApplication.class, args);
+	}
+
+}
+
